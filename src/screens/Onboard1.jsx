aea73acab81326d7e79b22a3794e8ad8b74d001b
Prince4Iyke <151050678+Prince4Iyke@users.noreply.github.com>
2026-07-29 05:27:25 +0100

    Onboard1.jsx

--- src/screens/Onboard1.jsx
+++ src/screens/Onboard1.jsx
@@ -42,10 +42,10 @@ export default function Onboard1() {
   
 
   return (
-    <div style={{ height: 670, display: "flex", flexDirection: "column", background: C.card, padding: "40px 22px 26px", overflowY: "auto" }}>
-      <BackHeader style={{ marginBottom: 1, }}  title="" onBack={goBack} />
+    <div style={{ margin: "0 auto 2px", height: "100%", display: "flex", flexDirection: "column", background: C.card, padding: "40px 22px 26px", overflowY: "auto" }}>
+      <BackHeader  title="" onBack={goBack} />
       <div style={{ flex: 1, display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center" }}>
-        <div style={{ width: "90%", borderRadius: 20, padding: 22, minHeight: 220, marginTop: 2, backgroundImage: "url(/images/visa-card-bg.jpg)", backgroundSize: "cover", backgroundPosition: "center", position: "relative", overflow: "hidden", boxShadow: "0 16px 30px -12px rgba(12,122,56,0.45)" }}>
+        <div style={{ margin: "0 auto 2px", width: "90%", borderRadius: 20, padding: 22, minHeight: 220, marginTop: 2, backgroundImage: "url(/images/visa-card-bg.jpg)", backgroundSize: "cover", backgroundPosition: "center", position: "relative", overflow: "hidden", boxShadow: "0 16px 30px -12px rgba(12,122,56,0.45)" }}>
           <div 
           style={{ 
             position: "absolute",
